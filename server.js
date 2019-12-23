176219576cd2b1d004dbbef7a25413e7b5d3ddea
Wenye Lu <luwenye2010@gmail.com>
2019-12-23 15:30:58 -0500

    ready for deploy

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -24,6 +25,16 @@ function validateToken(req, res, next){
 }
 
 app.use(validateToken)
+
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 app.get('/movie', (req, res) => {
     const {genre, country, avg_vote} = req.query
@@ -48,7 +59,7 @@ app.get('/movie', (req, res) => {
     res.json(results)
 })
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Server listening a http://localhost:${PORT}`)
 })
